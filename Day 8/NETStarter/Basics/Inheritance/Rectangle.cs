835aa5b63c70f969f76bdb652ec9805c0df32d81
kabita paudel <kabitap491@gmail.com>
2024-04-12 11:49:33 +0545

    findind area and perimeter of rectangle, circle and square.

---
+++ Day 8/NETStarter/Basics/Inheritance/Rectangle.cs
@@ -0,0 +1,22 @@
+//write console app to calculate area and perimeter of different 20 shapes.
+class Rectangle
+{
+    //deafult parameterless constructor
+    // public rectangle()
+    // {
+
+    // }
+    //parameterized constructor
+    public Rectangle(double l, double w)
+    {
+        length = l;
+        width= w;
+    }
+    
+    double length;
+    double width;
+
+    public double GetArea() => length * width;
+    public double GetPerimeter() => 2 * (length + width);
+    
+}
--- Day 8/NETStarter/Basics/Program.cs
+++ Day 8/NETStarter/Basics/Program.cs
@@ -15,20 +15,40 @@
 
 // Console.WriteLine($"{Kabita.name} {Kabita.address} {Kabita.roll} {Kabita.dob}");
 
-Animal a1=new();
-Animal a2=new();
+// Animal a1=new();
+// Animal a2=new();
 
-a1.name="cow";
-a1.type="domestic";
-a1.scientificname= "Bos taurus";
-a1.weight=30;
-a1.age=10;
-Console.WriteLine($"{a1.name} {a1.type} {a1.scientificname} {a1.weight} {a1.age}");
+// a1.name="cow";
+// a1.type="domestic";
+// a1.scientificname= "Bos taurus";
+// a1.weight=30;
+// a1.age=10;
+// Console.WriteLine($"{a1.name} {a1.type} {a1.scientificname} {a1.weight} {a1.age}");
 
-a2.name="lion";
-a2.type=" non domestic";
-a2.scientificname= " Panthera leo";
-a2.weight=20;
-a2.age=5;
-Console.WriteLine($"{a2.name} {a2.type} {a2.scientificname} {a2.weight} {a2.age}");
+// a2.name="lion";
+// a2.type=" non domestic";
+// a2.scientificname= " Panthera leo";
+// a2.weight=20;
+// a2.age=5;
+// Console.WriteLine($"{a2.name} {a2.type} {a2.scientificname} {a2.weight} {a2.age}");
 
+// var rec1 = new Rectangle(18.18,8.8);
+// var p = rec1.GetPerimeter();
+
+// var rec2 = new Rectangle(4.5,5.5);
+// var a = rec2.GetArea();
+// Console.WriteLine("Rectangle 1 - Perimeter:" + p);
+// Console.WriteLine("Rectangle 2 - Area:" + a);
+
+// var square1 = new Square(9.8);
+// var sa = square1.GetArea();
+// var sp = square1.GetPerimeter();
+// Console.WriteLine("Square1 -perimeter: " + sp);
+// Console.WriteLine("Square1 -Area: " + sa);
+
+
+Circle c1 = new Circle(16.3);
+double perimeter = c1.GetPerimeter();
+double area = c1.GetArea();
+Console.WriteLine("circle-Perimeter: " + perimeter);
+Console.WriteLine("circle-Area: " + area);
